6a9cf5e0b0fbfcb9b12cff6cec173e7a383d8bc7
Prithviraj97 <prithvirajsingh358@gmail.com>
2024-02-15 22:54:14 -0600

    some progress

--- Navraj/check.py
+++ Navraj/check.py
@@ -4,8 +4,8 @@ from PIL import Image, ImageTk
 import os
 MAX_CARD_WIDTH = 50  # Maximum width for the card images (adjust as needed)
 CARD_HEIGHT_RATIO = 1.4  # Aspect ratio of the card images (height / width)
-MAX_WIDTH = 400  # Maximum width for the card image
-MAX_HEIGHT = 526
+MAX_WIDTH = 500  # Maximum width for the card image
+MAX_HEIGHT = 726
 PADDING = 10
 class CardGameGUI:
 
@@ -19,17 +19,24 @@ class CardGameGUI:
         self.card_images = {}
         self.card_instances = []
         self.load_card_images()
-        self.play_game()
+        # self.play_game()
         
+        self.default_image_path = "Navraj\CardGame\images\default.png"
+        self.default_image = self.load_default_image()
+
+        # Display default images for player and computer cards
+        self.default_card(self.default_image, 60, 40)  # Player card
+        self.default_card(self.default_image, 620, 40)
+
 
         self.play_button = tk.Button(root, text="Play", command=self.play_game)
         self.play_button.pack(side=tk.LEFT, padx=PADDING)
 
         self.restart_button = tk.Button(root, text="Restart", justify="left", command=self.restart)
-        self.restart_button.pack(side=tk.LEFT)
+        self.restart_button.pack(side=tk.LEFT, padx=PADDING)
 
         self.quit_button = tk.Button(root, text="Quit", justify="right", command=self.quit_game)
-        self.quit_button.pack(side=tk.RIGHT)
+        self.quit_button.pack(side=tk.RIGHT, padx=PADDING)
 
         self.result_label = tk.Label(root, text="")
         self.result_label.pack()
@@ -47,9 +54,7 @@ class CardGameGUI:
         # self.default_image = self.default_image.resize((min(self.default_image.width, MAX_WIDTH), min(self.default_image.height, MAX_HEIGHT)))
         # self.default_tk_image = ImageTk.PhotoImage(self.default_image)
         # Load the default card image
-        self.default_image_path = "Navraj\CardGame\images\default.png"
-        self.default_image = self.load_default_image()
-
+        
     def load_card_images(self):
         for suit in Card.POSSIBLESUITS:
             for number in range(2, 11):
@@ -63,12 +68,13 @@ class CardGameGUI:
     def load_default_image(self):
         try:
             default_image = Image.open(self.default_image_path)
-            # default_image = default_image.resize((min(default_image.width, MAX_WIDTH), min(default_image.height, MAX_HEIGHT)))
+            default_image = default_image.resize((min(default_image.width, MAX_WIDTH), min(default_image.height, MAX_HEIGHT)))
             return ImageTk.PhotoImage(default_image)
         except FileNotFoundError:
             return None
         
     def play_game(self):
+        
         if self.game.deck.size() < 2:
             self.canvas.create_text(580, 790, text="Not enough cards to play.")
             self.root.update()
@@ -101,23 +107,22 @@ class CardGameGUI:
         image = self.card_images.get((number, suit))
         self.canvas.create_image(x, y, image=image, anchor=tk.NW)
         
-        
-    # def restart(self, photo):
-    #     self.game = Game()
-    #     self.result_label.config(text="")
-    #     # photo = "C:\\Users\\TheEarthG\\Downloads\\ForageCognizant\\Navraj\\CardGame\\images\\default.png"
-    #     self.player_card_label.config(image=photo)
+    def default_card(self, image, x, y):
+        if image:
+            self.canvas.create_image(x, y, image=image, anchor=tk.NW)  
+
+ 
     def restart(self):
         self.play_button.config(command=lambda: None)
         self.game = Game()  
-        self.canvas.delete("all")  # Clear the canvas
+        self.canvas.delete("all")  
 
         self.load_card_images()
         # Load the default card image
         image_path = "Navraj\CardGame\images\default.png"
         image = Image.open(image_path)
         image = image.resize((min(image.width, MAX_WIDTH), min(image.height, MAX_HEIGHT)))
-        default_image = ImageTk.PhotoImage(image)
+        self.default_image = ImageTk.PhotoImage(image)
         
         # Display the default card for both player and computer
         if self.default_image:
